9599027af8718187468fa25b236a84692038b5d6
Josphze <43673761+Josphze@users.noreply.github.com>
2020-04-20 20:20:28 +0100

    Jgoo

--- Code/Javascript/projeto.js
+++ Code/Javascript/projeto.js
@@ -9,7 +9,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 20 },
             debug: false
         }
     },
@@ -29,6 +29,10 @@ function preload(){
     this.load.spritesheet('padeira_idle', 'Resources/Sprite Sheets/Padeira/Padeira_idle.png', { frameWidth: 72, frameHeight: 108 });
     this.load.spritesheet('padeira_walk_R', 'Resources/Sprite Sheets/Padeira/Padeira_walk_R.png', { frameWidth: 72, frameHeight: 108 });
     this.load.spritesheet('padeira_walk_L', 'Resources/Sprite Sheets/Padeira/Padeira_walk_L.png', { frameWidth: 72, frameHeight: 108 });
+    this.load.spritesheet('padeira_jump_R', 'Resources/Sprite Sheets/Padeira/Padeira_jump_R.png', { frameWidth: 72, frameHeight: 108 });
+    this.load.spritesheet('padeira_jump_L', 'Resources/Sprite Sheets/Padeira/Padeira_jump_L.png', { frameWidth: 72, frameHeight: 108 });
+    this.load.spritesheet('padeira_fall_R', 'Resources/Sprite Sheets/Padeira/Padeira_fall_R.png', { frameWidth: 72, frameHeight: 108 });
+    this.load.spritesheet('padeira_fall_L', 'Resources/Sprite Sheets/Padeira/Padeira_fall_L.png', { frameWidth: 72, frameHeight: 108 });
 }
 
 
@@ -55,7 +59,7 @@ function create(){
     this.anims.create({
         key: 'idle',
         frames: this.anims.generateFrameNumbers('padeira_idle', { start: 0, end: 3 }),
-        frameRate: 10,
+        frameRate: 7,
         repeat: -1
     });
 
@@ -66,41 +70,104 @@ function create(){
         repeat: -1
     });
 
+    this.anims.create({
+        key: 'jump_R',
+        frames: this.anims.generateFrameNumbers('padeira_jump_R', { start: 0, end: 3 }),
+        frameRate: 20,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'jump_L',
+        frames: this.anims.generateFrameNumbers('padeira_jump_L', { start: 3, end: 0 }),
+        frameRate: 20,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'fall_R',
+        frames: this.anims.generateFrameNumbers('padeira_fall_R', { start: 0, end: 1 }),
+        frameRate: 20,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'fall_L',
+        frames: this.anims.generateFrameNumbers('padeira_fall_L', { start: 1, end: 0 }),
+        frameRate: 20,
+        repeat: -1
+    });
+
     cursors = this.input.keyboard.createCursorKeys();
     
     this.physics.add.collider(player, platforms);
 }
 
+var atual // true = direita, false = esquerda
 
-function update ()
-{
+function update (){
+
+
     if (gameOver)
-    {
         return;
-    }
 
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown){
         player.setVelocityX(-300);
 
-        player.anims.play('left', true);
+        if (player.body.touching.down)
+            player.anims.play('left', true);
     }
-    else if (cursors.right.isDown)
-    {
+
+    else if (cursors.right.isDown){
         player.setVelocityX(300);
 
-        player.anims.play('right', true);
+        if (player.body.touching.down)
+            player.anims.play('right', true);
     }
-    else
-    {
-        player.setVelocityX(0);
 
+    else if (player.body.touching.down){
+        player.setVelocityX(0);
         player.anims.play('idle', true);
     }
+    
+    if (cursors.up.isDown && player.body.touching.down){
 
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        console.log("salto activate")
-        player.setVelocityY(-330);
+        player.setVelocityY(-300);
+        
+        if (player.body.velocity.x >= 0){
+            player.anims.play('jump_R', true);
+            atual = true;
+        }
+        
+        else{
+            player.anims.play('jump_L', true);
+            atual = false;
+        }
+    }
+    
+    else if (!player.body.touching.down){
+
+        if (player.body.velocity.y >= 0){
+
+            if (player.body.velocity.x >= 0){
+                player.anims.play('fall_R', true);
+            }
+
+            else
+                player.anims.play('fall_L', true);
+        }
+
+        else{
+
+            if (!atual && player.body.velocity.x >= 0){
+                player.anims.play('jump_R', false);
+                atual = true;
+            }
+
+            else if (atual && player.body.velocity.x < 0){
+                player.anims.play('jump_L', false);
+                atual = false;
+            }
+        }
     }
 }
