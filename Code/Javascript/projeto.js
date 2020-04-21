38f05eada0cb98cd8b3d5367b9cb559207f7cb65
Josphze <43673761+Josphze@users.noreply.github.com>
2020-04-21 17:26:37 +0100

    Attack animations

--- Code/Javascript/projeto.js
+++ Code/Javascript/projeto.js
@@ -9,7 +9,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 10 },
+            gravity: { y: 1550 },
             debug: false
         }
     },
@@ -22,18 +22,30 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+
 function preload(){
     
     this.load.image('sky', 'Resources/Sprites/Temp Assets/bck.png');
     this.load.image('ground', 'Resources/Sprites/Temp Assets/platform.png');
-    this.load.spritesheet('padeira_idle', 'Resources/Sprite Sheets/Padeira/Padeira_idle.png', { frameWidth: 72, frameHeight: 108 });
-    this.load.spritesheet('padeira_walk_R', 'Resources/Sprite Sheets/Padeira/Padeira_walk_R.png', { frameWidth: 72, frameHeight: 108 });
-    this.load.spritesheet('padeira_walk_L', 'Resources/Sprite Sheets/Padeira/Padeira_walk_L.png', { frameWidth: 72, frameHeight: 108 });
-    this.load.spritesheet('padeira_jump_R', 'Resources/Sprite Sheets/Padeira/Padeira_jump_R.png', { frameWidth: 72, frameHeight: 108 });
-    this.load.spritesheet('padeira_jump_L', 'Resources/Sprite Sheets/Padeira/Padeira_jump_L.png', { frameWidth: 72, frameHeight: 108 });
-    this.load.spritesheet('padeira_fall_R', 'Resources/Sprite Sheets/Padeira/Padeira_fall_R.png', { frameWidth: 72, frameHeight: 108 });
-    this.load.spritesheet('padeira_fall_L', 'Resources/Sprite Sheets/Padeira/Padeira_fall_L.png', { frameWidth: 72, frameHeight: 108 });
+
+    this.load.spritesheet('padeira_idle', 'Resources/Sprite Sheets/Padeira/Padeira_idle.png', { frameWidth: 72, frameHeight: 140 });
+    this.load.spritesheet('padeira_walk_R', 'Resources/Sprite Sheets/Padeira/Padeira_walk_R.png', { frameWidth: 72, frameHeight: 140 });
+    this.load.spritesheet('padeira_walk_L', 'Resources/Sprite Sheets/Padeira/Padeira_walk_L.png', { frameWidth: 72, frameHeight: 140 });
+    this.load.spritesheet('padeira_jump_R', 'Resources/Sprite Sheets/Padeira/Padeira_jump_R.png', { frameWidth: 64, frameHeight: 140 });
+    this.load.spritesheet('padeira_jump_L', 'Resources/Sprite Sheets/Padeira/Padeira_jump_L.png', { frameWidth: 64, frameHeight: 140 });
+    this.load.spritesheet('padeira_fall_R', 'Resources/Sprite Sheets/Padeira/Padeira_fall_R.png', { frameWidth: 68, frameHeight: 140 });
+    this.load.spritesheet('padeira_fall_L', 'Resources/Sprite Sheets/Padeira/Padeira_fall_L.png', { frameWidth: 68, frameHeight: 140 });
     this.load.spritesheet('padeira_fall_P', 'Resources/Sprite Sheets/Padeira/Padeira_fall_P.png', { frameWidth: 68, frameHeight: 104 });
+
+    this.load.spritesheet('padeira_weapon_out_R', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_out_R.png', { frameWidth: 124, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_out_L', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_out_L.png', { frameWidth: 124, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_walk_R', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_walk_R.png', { frameWidth: 124, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_walk_L', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_walk_L.png', { frameWidth: 124, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_idle', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_idle.png', { frameWidth: 124, frameHeight: 140 }); 
+    this.load.spritesheet('padeira_weapon_jump_R', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_jump_R.png', { frameWidth: 136, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_jump_L', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_jump_L.png', { frameWidth: 136, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_fall_R', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_fall_R.png', { frameWidth: 124, frameHeight: 140 });
+    this.load.spritesheet('padeira_weapon_fall_L', 'Resources/Sprite Sheets/Padeira/Padeira_weapon_fall_L.png', { frameWidth: 124, frameHeight: 140 });
 }
 
 
@@ -45,9 +57,9 @@ function create(){
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-    player = this.physics.add.sprite(100, 450, 'padeira_idle');
-
-    player.setBounce(0.2);
+    player = this.physics.add.sprite(100, 400, 'padeira_idle');
+    
+    player.setBounce(0);
     player.setCollideWorldBounds(true);
 
     this.anims.create({
@@ -61,7 +73,7 @@ function create(){
         key: 'idle',
         frames: this.anims.generateFrameNumbers('padeira_idle', { start: 0, end: 3 }),
         frameRate: 7,
-        repeat: -1
+        repeat: 0
     });
 
     this.anims.create({
@@ -106,53 +118,165 @@ function create(){
         repeat: 0
     });
 
+    this.anims.create({
+        key: 'w_out_R',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_out_R', { start: 0, end: 14 }),
+        frameRate: 40,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'w_out_L',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_out_L', { start: 14, end: 0 }),
+        frameRate: 40,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'w_in_R',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_out_R', { start: 14, end: 0 }),
+        frameRate: 40,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'w_in_L',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_out_L', { start: 0, end: 14 }),
+        frameRate: 40,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'w_idle',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_idle', { start: 0, end: 3 }),
+        frameRate: 7,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'w_right',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_walk_R', { start: 0, end: 5 }),
+        frameRate: 15,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'w_left',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_walk_L', { start: 0, end: 5 }),
+        frameRate: 15,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'w_jump_R',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_jump_R', { start: 0, end: 3 }),
+        frameRate: 20,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'w_jump_L',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_jump_L', { start: 3, end: 0 }),
+        frameRate: 20,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'w_fall_R',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_fall_R', { start: 0, end: 1 }),
+        frameRate: 20,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'w_fall_L',
+        frames: this.anims.generateFrameNumbers('padeira_weapon_fall_L', { start: 0, end: 1 }),
+        frameRate: 20,
+        repeat: -1
+    });
+
     cursors = this.input.keyboard.createCursorKeys();
-    
+    spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
     this.physics.add.collider(player, platforms);
 }
 
 var atual // true = direita, false = esquerda
 var pico = false
+var weapon = false
+
+
+function toggleWeapon(){
+
+    if (weapon)
+        weapon = false;
+    else
+        weapon = true;
+}
+
+var start_time = performance.now();
 
 function update (){
 
+    var time = performance.now() - start_time;
 
     if (gameOver)
         return;
-
+    
     if (cursors.left.isDown){
-        player.setVelocityX(-300);
+        player.setVelocityX(-350);
 
         if (player.body.touching.down)
-            player.anims.play('left', true);
+            if (!weapon)
+                player.anims.play('left', true);
+            else
+                player.anims.play('w_left', true);
+
+        start_time = performance.now()
     }
 
     else if (cursors.right.isDown){
-        player.setVelocityX(300);
+        player.setVelocityX(350);
 
         if (player.body.touching.down)
-            player.anims.play('right', true);
+            if (!weapon)
+                player.anims.play('right', true);
+            else
+                player.anims.play('w_right', true);
+
+        start_time = performance.now()
     }
 
     else if (player.body.touching.down){
         player.setVelocityX(0);
-        player.anims.play('idle', true);
+        if (!weapon)
+            player.anims.play('idle', true);
+        else
+            player.anims.play('w_idle', true);
         pico = false;
+        start_time = performance.now()
     }
     
     if (cursors.up.isDown && player.body.touching.down){
 
-        player.setVelocityY(-600);
+        player.setVelocityY(-650);
         
         if (player.body.velocity.x >= 0){
-            player.anims.play('jump_R', true);
+            if (!weapon)
+                player.anims.play('jump_R', true);
+            else
+                player.anims.play('w_jump_R', true);
             atual = true;
         }
         
         else{
-            player.anims.play('jump_L', true);
+            if (!weapon)
+                player.anims.play('jump_L', true);
+            else
+                player.anims.play('w_jump_L', true);
             atual = false;
         }
+        start_time = performance.now()
     }
     
     else if (!player.body.touching.down){
@@ -160,28 +284,64 @@ function update (){
         if (player.body.velocity.y >= 0){
 
             if (player.body.velocity.x >= 0){
-            	if(pico == false){
+            	if (pico == false && weapon){
             		player.anims.play('fall_P',false);
             		pico = true;
-            	}
-                player.anims.play('fall_R', true);
+                }
+                if (!weapon)
+                    player.anims.play('fall_R', true);
+                else
+                    player.anims.play('w_fall_R', true);
             }
 
             else
-                player.anims.play('fall_L', true);
+                if (!weapon)
+                    player.anims.play('fall_L', true);
+                else
+                    player.anims.play('w_fall_L', true);
         }
 
         else{
 
             if (!atual && player.body.velocity.x >= 0){
-                player.anims.play('jump_R', false);
+                if (!weapon)
+                    player.anims.play('jump_R', false);
+                else
+                    player.anims.play('w_jump_R', true);
                 atual = true;
             }
 
             else if (atual && player.body.velocity.x < 0){
-                player.anims.play('jump_L', false);
+                if (!weapon)
+                    player.anims.play('jump_L', false);
+                else
+                    player.anims.play('w_jump_L', true);
                 atual = false;
             }
         }
+        start_time = performance.now()
     }
+
+    if (Phaser.Input.Keyboard.JustDown(spacebar)){
+        if (weapon){
+            toggleWeapon();
+            
+            if (player.body.velocity.x >= 0)
+                player.anims.play('w_in_R', true);
+            else
+                player.anims.play('w_in_L', true);
+        }
+        
+        else if (!weapon){
+            toggleWeapon();
+            
+            if (player.body.velocity.x >= 0)
+                player.anims.play('w_out_R', false);
+            else
+                player.anims.play('w_out_L', false);
+        }
+        
+        console.log(weapon, player.body.velocity.x);
+        start_time = performance.now()
+    }   
 }
