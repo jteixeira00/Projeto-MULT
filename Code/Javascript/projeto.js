57a3f9ae73f4dfbe150d2393a0f512e1d5f7f5df
Josphze <43673761+Josphze@users.noreply.github.com>
2020-05-22 18:08:15 +0100

    Classe PauseScene

--- Code/Javascript/projeto.js
+++ Code/Javascript/projeto.js
@@ -131,7 +131,6 @@ function preload(){
     this.load.spritesheet('portal_op', '../../Resources/Sprite Sheets/Portal/portal_open.png',{ frameWidth: 232, frameHeight: 156 });
     this.load.spritesheet('portal_ed', '../../Resources/Sprite Sheets/Portal/portal_close.png',{ frameWidth: 277, frameHeight: 156 });
 
-
     this.load.image("minicarro",'../../Resources/Sprites/Jogo/lvl1/carrinho drop.png' );
 
     this.load.spritesheet('health', '../../Resources/Sprite Sheets/HUD/health bread.png',{ frameWidth: 88, frameHeight: 16 });
@@ -700,47 +699,6 @@ function pause(){
 }
 
 
-class PauseScene extends Phaser.Scene{
-
-    constructor(configMenu){
-        super(configMenu);
-
-    }
-    preload(){
-        this.load.image("menu", '../../Resources/Sprites/Jogo/Pause/menu.png')
-        this.load.image("gram+","../../Resources/Sprites/Jogo/Pause/gramof +.png" );
-        this.load.image("gram-","../../Resources/Sprites/Jogo/Pause/gramof -.png" );
-        this.load.spritesheet("volume", "../../Resources/Sprites/Jogo/Pause/volume-sheet.png", {frameWidth: 277, frameHeight: 64});
-    }
-
-    create(){
-        var btn = this.add.image(600, 400, "menu");
-        btn.setScrollFactor(0)
-        btn.setInteractive();
-        btn.on("pointerdown", () => hideMenu());
-
-        var gramMais = this.add.image(805, 350+40, "gram+").setScale(0.8);
-        var gramMenos = this.add.image(390, 380+40, "gram-");
-        volume = this.add.sprite(585,400+40, "volume");
-
-        gramMais.setInteractive();
-        gramMais.on("pointerdown", () => updateVolume(1));
-        gramMenos.setInteractive();
-        gramMenos.on("pointerdown", () => updateVolume(-1));
-            
-        this.anims.create({
-            key: "volume",
-            frames: this.anims.generateFrameNumbers("volume", {start:0, end: 14}),
-            frameRate:1,
-            repeat:-1   
-        });
-
-        volume.anims.play("volume", true);
-        volume.anims.pause(volume.anims.currentAnim.frames[volumeFrame]);
-    }
-}
-
-
 function updateVolume(change){
     
     if (change==1){
@@ -848,7 +806,6 @@ function updatePadeira(scene){
                 padeira.anims.play('padeira_weapon_walk_L', true);
             }
         }
-
     }
 
     else if (cursors.right.isDown && !padeira.immobile){
